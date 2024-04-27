9d94de28057fea3cda3630001cdc6de09cbee353
alisonparkdev <aka.parker@gmail.com>
2024-04-27 16:28:45 -0700

    guesses functioning

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int chosenNumber = randomGenerator.Next(1, 11);
+        int guessNumber = -1;
+
+        while (guessNumber != chosenNumber)
+        { 
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber < chosenNumber)
+            {
+            Console.WriteLine("Higher");
+            }
+
+            else if (guessNumber > chosenNumber)
+            {
+            Console.WriteLine("Lower");
+            }
+            else
+            Console.WriteLine("You guessed it!");
+        }
+
+        
     }
 }
