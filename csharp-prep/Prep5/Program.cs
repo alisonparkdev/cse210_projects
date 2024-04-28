06e39b802dcb56e0bf3a5d597127626dc2b11fe1
alisonparkdev <aka.parker@gmail.com>
2024-04-27 20:35:33 -0700

    working functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    {   
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favNumber = UserNumber();
+        int square = SquareNumber(favNumber);
+        DisplayResult(name, square);
+
+        static string DisplayWelcome ()
+        {
+            string welcome = "Welcome to the Program";
+            return welcome;
+        
+        }
+
+        static string PromptUserName ()
+        {
+            Console.Write("What is your name? ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int UserNumber ()
+        {
+            Console.Write("What is your favorite number? ");
+            int favNumber = int.Parse(Console.ReadLine());
+            return favNumber;
+        }
+        static int SquareNumber (int favNumber)
+        {
+            int square = favNumber * favNumber;
+            return square;
+        }
+        static void DisplayResult (string userName, int square)
+        {
+            Console.WriteLine($"{userName}, your number squared is {square}.");
+        }
+
+
+
     }
 }
