b6a06ae0b34007a1f993a9df30bc78b49c446e14
alisonparkdev <aka.parker@gmail.com>
2024-04-27 19:54:57 -0700

    List functions added

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int newNumber = 1;
+
+        while (newNumber != 0)
+        {
+    
+            Console.Write("Type a number: ");
+            string userNumber = Console.ReadLine();
+            newNumber = int.Parse(userNumber);
+
+
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+            
+        }
+     
+    int sum = numbers.Sum();
+    double avg = numbers.Average();
+    int max = numbers.Max();
+
+    Console.WriteLine($"The sum is {sum}.");
+    Console.WriteLine($"The average is {avg}.");
+    Console.WriteLine($"The largest number is {max}.");
     }
 }
